012b8bfff05e3d64b99fda0551d75c820bec4100
nobuhiroharada <nobuhiroharada@hotmail.com>
2020-04-16 21:08:27 +0900

    vueコンポーネント、vue-router設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('{any}', 'AppController@index')
+    ->where('any', '.*')
+    ->middleware('auth')
+    ->name('home');
