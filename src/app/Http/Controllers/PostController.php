fe5cbc5ddddf629bb8b87441372daeea50a3309f
nobuhiroharada <nobuhiroharada@hotmail.com>
2020-04-18 07:50:57 +0900

    投稿テスト追加
    PoseResource, UserResource追加

---
+++ src/app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use App\Http\Resources\Post as PostResource;
+
+class PostController extends Controller
+{
+    public function store()
+    {
+        $data = request()->validate([
+            'data.attributes.body' => '',
+        ]);
+
+        $post = request()->user()->posts()->create($data['data']['attributes']);
+
+        return new PostResource($post);
+    }
+}
