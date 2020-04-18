77d263bcee558e8ecee0f8434a1d0c8abd4e27df
nobuhiroharada <nobuhiroharada@hotmail.com>
2020-04-19 02:05:42 +0900

    投稿一覧テスト追加

--- src/app/Http/Controllers/PostController.php
+++ src/app/Http/Controllers/PostController.php
@@ -5,9 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use App\Http\Resources\Post as PostResource;
+use App\Http\Resources\PostCollection;
 
 class PostController extends Controller
 {
+    public function index()
+    {
+        return new PostCollection(request()->user()->posts);
+    }
+
     public function store()
     {
         $data = request()->validate([
